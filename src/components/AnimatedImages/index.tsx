73e8b6c6b0665367c409972a3d71859af91395ec
emmypuch <adesuyiomowunmi@gmail.com>
2025-06-17 23:51:57 +0100

    modified sub hero section

--- src/components/AnimatedImages/index.tsx
+++ src/components/AnimatedImages/index.tsx
@@ -3,9 +3,18 @@
 import React from "react";
 import Image from "next/image";
 import { motion } from "framer-motion";
-import AnimatedImagesStyles from "./AnimatedImages.module.scss";
 import cloudinaryLoader from "@/lib/cloudinaryLoader";
 import { imageData } from "@/data/imageData";
+import styles from "./AnimatedImages.module.scss";
+
+const POSITION_OVERRIDES = [
+  { x: -300, y: -150, rotate: -5 },
+  { x: -80, y: 80, rotate: 2 },
+  { x: -310, y: 250, rotate: -8 },
+  { x: 30, y: 10, rotate: 5 },
+  { x: 100, y: -10, rotate: -3 },
+  { x: 180, y: 15, rotate: 7 },
+];
 
 interface AnimatedImagesProps {
   count?: number;
@@ -16,38 +25,43 @@ const AnimatedImages = ({ count = 6, startIndex = 0 }: AnimatedImagesProps) => {
   const imagesToShow = imageData.slice(startIndex, startIndex + count);
 
   return (
-    <div className={AnimatedImagesStyles.wrapper}>
-      {imagesToShow.map((image, index) => (
-        <motion.div
-          key={index}
-          initial={{ x: image.x, y: image.y, rotate: image.rotate }}
-          animate={{
-            x: [image.x, image.x + 5, image.x],
-            y: [image.y, image.y + 5, image.y],
-            rotate: [image.rotate, image.rotate + 2, image.rotate],
-          }}
-          transition={{
-            duration: 4 + Math.random() * 3,
-            repeat: Infinity,
-            repeatType: "reverse",
-            ease: "easeInOut",
-            delay: index * 0.2,
-          }}
-          style={{
-            position: "absolute",
-            zIndex: 10,
-          }}
-        >
-          <Image
-            loader={cloudinaryLoader}
-            src={image.src}
-            alt={image.alt}
-            width={152}
-            height={152}
-            unoptimized
-          />
-        </motion.div>
-      ))}
+    <div className={styles.wrapper}>
+      {imagesToShow.map((image, index) => {
+        const position = POSITION_OVERRIDES[index] || { x: 0, y: 0, rotate: 0 };
+
+        return (
+          <motion.div
+            key={index}
+            initial={{
+              x: position.x,
+              y: position.y,
+              rotate: position.rotate,
+            }}
+            animate={{
+              x: [position.x, position.x + 5, position.x],
+              y: [position.y, position.y + 5, position.y],
+              rotate: [position.rotate, position.rotate + 2, position.rotate],
+            }}
+            transition={{
+              duration: 4 + Math.random() * 3,
+              repeat: Infinity,
+              repeatType: "reverse",
+              ease: "easeInOut",
+              delay: index * 0.2,
+            }}
+            className={styles.imageContainer}
+          >
+            <Image
+              loader={cloudinaryLoader}
+              src={image.src}
+              alt={image.alt}
+              width={152}
+              height={152}
+              unoptimized
+            />
+          </motion.div>
+        );
+      })}
     </div>
   );
 };
